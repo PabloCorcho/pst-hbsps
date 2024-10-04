431b554d1fbe534235f95b03187d43692e8a07f9
Pablo Corcho-Caballero <p.corcho.caballero@rug.nl>
2024-10-04 15:53:47 +0200

    add tests of pipeline module

---
+++ tests/cosmosis_datablock.py
@@ -0,0 +1,72 @@
+import sys
+from cosmosis import DataBlock
+
+# sys.path.append("../pipeline_modules/.")
+# import KinDust
+from hbsps.pipeline_modules.kin_dust import KinDustModule
+from hbsps.pipeline_modules.sfh_spectra import SFHSpectraModule
+
+kin_configuration = {
+    "KinDust": {
+        "save_ssp": "/path/to/ssp_output.txt",
+        "file": "/home/pcorchoc/Develop/HBSPS/KinDust.py",
+        "redshift": 0.0,
+        "inputSpectrum": "/home/pcorchoc/Develop/tutorial-pst-hbsps/generate_mock_data/exponential/input_spectra.dat",
+        "SSPModel": "PyPopStar",
+        "SSPModelArgs": "KRO",
+        "SSPDir": "None",
+        # "SSP-NMF": True, 
+        # "SSP-NMF-N": 10,
+        "SSPSave": "T",
+        "wlRange": [3700.0, 8000.0],
+        "wlNormRange": [5000.0, 5500.0],
+        "velscale": 70.0,
+        "oversampling": 2,
+        "polOrder": 10,
+        "ExtinctionLaw": "ccm89",
+    }
+}
+
+block = DataBlock()
+block['parameters', 'av'] = 0
+block['parameters', 'los_vel'] = 0
+block['parameters', 'los_sigma'] = 100.
+block['parameters', 'los_h3'] = 0
+block['parameters', 'los_h4'] = 0
+
+# kindust_module = KinDustModule(kin_configuration)
+# kindust_module.execute(block)
+
+config = {"SFH_spectra": {
+        "file": "/home/pcorchoc/Develop/HBSPS/SFH_stellar_mass.py",
+        "redshift": 0.0,
+        "inputSpectrum": "/home/pcorchoc/Develop/tutorial-pst-hbsps/generate_mock_data/exponential/input_spectra.dat",
+        "SSPModel": "PyPopStar",
+        "SSPModelArgs": "KRO",
+        "SSPDir": "None",
+        # "SSP-NMF": "F",
+        # "SSP-NMF-N": None,
+        #"SSPSave": "F",
+        "wlRange": [3700.0, 6000.0],
+        "wlNormRange": [5000.0, 5500.0],
+        # "SFHModel": "LogNormalQuenchedSFH",
+        # "SFHModel": "LogNormalSFH",
+        "SFHModel": "FixedMassFracSFH",
+        "SFHArgs1": [0.1, 0.3, 0.5, 0.7, 0.9, 0.95, 0.99],
+        "velscale": 70.0,
+        "oversampling": 2,
+        "polOrder": 10,
+        "los_vel": 0.0,
+        "los_sigma": 100.0,
+        "av": 0.0,
+        "ExtinctionLaw": "ccm89",
+    }}
+
+block = DataBlock()
+block['parameters', 'av'] = 0
+block['parameters', 'los_vel'] = 0
+block['parameters', 'los_sigma'] = 100.
+block['parameters', 'los_h3'] = 0
+block['parameters', 'los_h4'] = 0
+
+sfh_module = SFHSpectraModule(config)
